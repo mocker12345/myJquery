437ba1b4fb4086a43b5a600ae5d49d814d71c264
rancongjie <95633676@qq.com>
2016-07-24 22:38:09 +0800

    first commit framework

---
+++ mj.js
@@ -0,0 +1,50 @@
+/**
+ * Created by rancongjie on 16/7/24.
+ */
+(function (window, document) {
+  var w = window;
+  var d = document;
+  var R = function (selector) {
+    return new R.prototype.init(selector)
+  };
+  R.prototype = {
+    constructor: R,
+    length: 0,
+    splice: [].splice,
+    selector: '',
+    init: function (selector) {
+      if (!selector) {
+        return selector;
+      }
+      var selector = selector.trim();
+      var elm;
+      if (selector.charAt(0) === '#' && !selector.match(/\s/)) {
+        selector = selector.substring(1);
+        this.selector = selector;
+        elm = d.getElementById(selector);
+        this.length = 1;
+        this[0] = elm;
+        return this;
+      } else {
+        elm = d.querySelectorAll(selector);
+        this.length = elm.length;
+        for (var i = 0; i < elm.length; i++) {
+          this[i] = elm[i];
+        }
+        this.selector = selector;
+        return this;
+      }
+    },
+    css: function (attr,val) {
+      for (var i = 0; i<this.length;i++){
+        if (arguments.length === 1){
+          return getComputedStyle(this[i],null)[attr];
+        }
+        this[i].style[attr] = val;
+      }
+      return this;
+    }
+  };
+  R.prototype.init.prototype = R.prototype;
+  window.R = R;
+})(window, document);
